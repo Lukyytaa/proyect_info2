b19adfe6d3c96d7cd0ca6e3bc2e4b23c0e76e121
Lukyytaa <A100245814@my.uvm.edu.mx>
2025-04-30 13:30:24 -0600

    mi_script.js

---
+++ mi_script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const boton = document.getElementById('miBoton');
+    boton.addEventListener('click', function () {
+        alert('¡Bienvenid@ a nuestro proyecto sobre el Robo de Información!');
+    });
+});
